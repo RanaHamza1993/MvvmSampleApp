daaec07302f9e24c7aee095d4e3ce00fe080e32e
Hafiz Hamza <hafiz.hamza@brainplow.com>
2019-11-07 16:30:24 +0500

    Initial commit

---
+++ app/src/main/java/com/brainplow/mvvmsampleapp/ui/auth/LoginActivity.kt
@@ -0,0 +1,13 @@
+package com.brainplow.mvvmsampleapp.ui.auth
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.brainplow.mvvmsampleapp.R
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
